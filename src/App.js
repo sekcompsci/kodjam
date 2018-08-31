283ed004b684db8eb1ee56ce5012d0c033d01b71
Thongchai <ais_arm@hotmail.com>
2018-09-01 03:00:07 +0700

    new feature

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import Button from './components/button';
-import logo from './logo.svg';
+import Footer from './components/footer';
+import Review from './components/box_review';
+import PostTimeline from './components/post_timeline'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <Button />
+          <PostTimeline></PostTimeline>
+          <Footer></Footer>
       </div>
     );
   }
