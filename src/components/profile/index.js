c03a9ecbc5844ba802b48a3315f33bdc81e03cfd
Sirawit Moonrinta <sirawit_m@cmu.ac.th>
2018-09-01 03:03:08 +0700

    add header

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import {Avatar, Card, Col, Row} from 'antd';
+import {TiArrowLeftThick, TiCogOutline} from "react-icons/ti";
+import './profile.css';
 
 import {createApp} from '../../actions/application';
 
 class Profile extends React.Component {
     render() {
-        return <div>Profile {this.props.command}</div>
+        return <div>
+            <header className="avatar-header">
+                <TiArrowLeftThick className="icon-app"/>
+                <TiCogOutline className="icon-app" style={{float: 'right'}}/>
+            </header>
+            <Row type="flex" justify="center">
+                <Col className="avatar"><Avatar shape="square" size={120} icon="user"/></Col>
+            </Row>
+            <div style={{textAlign: 'center', paddingTop: '5px'}}>
+                <div className="profile-name">{this.props.name}</div>
+                <div className="profile-description">ฝันอะไรไว้หลากหลาย เป็นอะไรก็ได้ที่อยากเป็น</div>
+            </div>
+            <Row gutter={8} style={{padding: '10px'}}>
+                <Col span={6}>
+                    <Card>Follow</Card>
+                </Col>
+                <Col span={6}>
+                    <Card>Favorite</Card>
+                </Col>
+                <Col span={6}>
+                    <Card>Test</Card>
+                </Col>
+                <Col span={6}>
+                    <Card>Test</Card>
+                </Col>
+            </Row>
+        </div>
     }
 }
 
@@ -15,12 +44,12 @@ Profile.propTypes = {
 };
 
 Profile.defaultProps = {
-    command: 'Sirawit Moonrinta'
+    name: 'สิรวิชญ์ มูลรินต๊ะ'
 };
 
 const mapStateToProps = state => {
     return {
-        command: state.command
+        name: state.name
     }
 };
 
