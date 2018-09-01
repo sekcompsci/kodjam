9f69b2b3e3f5b3425cba36f0fe77d2ef07cd49ec
Thongchai <ais_arm@hotmail.com>
2018-09-01 19:58:00 +0700

    search mock

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/footer';
 // import Review from './components/box_review';
 import PostTimeline from './components/post/post_timeline'
 import PostReview from './components/post/post_review'
+import SearchPage from './components/search'
 import Header from './components/header'
 import './App.scss';
 
@@ -11,8 +12,9 @@ class App extends Component {
     return (
       <div className="App">
           <Header typeBar="main" icon={true} title="Timeline"></Header>
-          <PostReview></PostReview>
+          {/* <PostReview></PostReview> */}
           {/* <PostTimeline></PostTimeline> */}
+          <SearchPage></SearchPage>
 
           <Footer typeBar="main" ForTimelineisFirst={false}></Footer>
       </div>
