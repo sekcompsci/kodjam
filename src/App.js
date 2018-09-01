0e503ac8065fbe9fb1eeafce4754c632560c980b
Thongchai <ais_arm@hotmail.com>
2018-09-01 14:16:00 +0700

    add app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
     return (
       <div className="App">
           <Header typeBar="search"></Header>
-          {/* <PostTimeline></PostTimeline> */}
           <PostReview></PostReview>
 
           <Footer typeBar="review" ForTimelineisFirst={true}></Footer>
