c97af0855009d498e43b7433eefcf3d140da48e2
Sirawit Moonrinta <sirawit_m@cmu.ac.th>
2018-09-01 20:27:18 +0700

    update login layout & edit route path

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -21,7 +21,7 @@ class Profile extends React.Component {
             <div>
                 <header className="avatar-header">
                     <TiArrowLeftThick className="icon-app" onClick={() => {
-                        this.props.history.push(`/login`)
+                        this.props.history.push(`/`)
                     }}/>
                     {this.props.own ? <TiCogOutline className="icon-app" style={{float: 'right'}}/> : ''}
                 </header>
@@ -82,16 +82,19 @@ class Profile extends React.Component {
                 <div style={{background: '#ECECEC', width: '100%', paddingTop: '1px', paddingBottom: '10vh'}}>
                     <ProfileFeed
                         img="https://images.unsplash.com/photo-1524260855046-f743b3cdad07?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bec431d5c80ae862ceba47454d36d9cc&auto=format&fit=crop&w=1701&q=80"
+                        profile={this.props.profile_picture}
                         type="ได้โพส ไทม์ไลน์"
                         description="ทริปลุยเขา เผาไฟป่า"
                     />
                     <ProfileFeed
                         img="https://images.unsplash.com/photo-1524027556923-66e7ec51e251?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2125b2ecf005515f7701153a086d4789&auto=format&fit=crop&w=1950&q=80"
+                        profile={this.props.profile_picture}
                         type="ได้โพส รีวิว"
                         description="เผาเสร็จแล้ว เที่ยวเขาหัวโล้นได้"
                     />
                     <ProfileFeed
                         img="https://images.unsplash.com/photo-1523978591478-c753949ff840?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjI0MX0&s=436a11a0fee324bde54ffd8d515c3ab1&auto=format&fit=crop&w=1950&q=80"
+                        profile={this.props.profile_picture}
                         type="ได้โพส ไทม์ไลน์"
                         description="มิชชันแก้ผ้าหน้าหนาว"
                     />
