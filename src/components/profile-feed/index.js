1f0360495873605406c7e9611085f36e9477edbc
zCaesar <suphawat_j@kkumail.com>
2018-09-02 02:12:41 +0700

    /firebaseInfo: /firebaseInfo:

--- src/components/profile-feed/index.js
+++ src/components/profile-feed/index.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import {Avatar, Card, Icon} from "antd";
+import firebase from 'firebase';
 
 class ProfileFeed extends React.Component {
     state = {
-        fav: false
+        fav: false,
+        count: 1,
+        username: null,
+        userimage: null,
+        follower_n: 0,
+        following_n: 0,
+        favorite: 0,
+        timeline: 0,
+        achievement: 0,
+        caption: "",
+        follow: false
     };
 
+    componentWillMount() {
+
+        let userpath = 'Users/' + 'bTFaYKdcpbYI94fbEsdtsLa8tbN2'; // userid (target)
+        let db_userpath = firebase.database().ref(userpath)
+
+        db_userpath.on('value', ss => {
+            this.setState({
+                username: ss.val().username,
+                userimage: ss.val().userimage,
+                follower_n: ss.val().follower_n,
+                following_n: ss.val().following_n,
+                favorite: ss.val().favorite,
+                timeline: ss.val().timeline,
+                achievement: ss.val().achievement,
+                caption: ss.val().caption,
+            })
+        })
+
+    }
+
     changeFav = () => {
+        let userpath = 'Users/' + 'bTFaYKdcpbYI94fbEsdtsLa8tbN2'; // userid (target)
+        let db_userpath = firebase.database().ref(userpath)
+
+        if (this.state.count % 2 === 0) {
+            db_userpath.set({
+                username: this.state.username,
+                userimage: this.state.userimage,
+                follower_n: this.state.follower_n,
+                following_n: this.state.following_n,
+                favorite: this.state.favorite - 1,
+                timeline: this.state.timeline,
+                achievement: this.state.achievement,
+                caption: this.state.caption,
+            })
+            this.setState({favorite: this.state.favorite - 1})
+        }
+        else {
+            db_userpath.set({
+                username: this.state.username,
+                userimage: this.state.userimage,
+                follower_n: this.state.follower_n,
+                following_n: this.state.following_n,
+                favorite: this.state.favorite + 1,
+                timeline: this.state.timeline,
+                achievement: this.state.achievement,
+                caption: this.state.caption,
+            })
+            this.setState({favorite: this.state.favorite + 1})
+        }
+        this.setState({count: this.state.count + 1})
         this.setState({fav: !this.state.fav});
     };
 
